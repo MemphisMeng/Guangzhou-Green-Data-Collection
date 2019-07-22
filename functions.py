e4441a7f4070c13f58f372a8323dc9a5bb9eab8e
Anzhe Meng <1310750076@qq.com>
2019-07-22 22:25:37 +0800

    General functions.py
    Another version of our widely utilized functions. But it still didn't last long.

--- functions.py
+++ functions.py
@@ -1,160 +1,253 @@
 # -*- coding: utf-8 -*-
 
+from scrapy.selector import Selector
+from collections import Iterable
 import re
-import hashlib
-import time
-import datetime
-import json
+from hpspider.items import HpspiderItem
+from functools import reduce
 
 
-def md5(name):
-    name = name.strip()  # Eliminate the blanks/spaces ahead of the real title
-    return hashlib.md5(name).hexdigest()
-
-
-def clean(i):
-    if i:
-        # return i.replace(' ', '').replace('\n', '').replace('\r', '').replace('\t', '')
-        return re.sub('\s', '', i)
+def match_file(files, i=None, data_rows=None, contents=None):
+    # Many websites are likely to put many cases in one page.
+    # Since we collected as many files as possible, we needed to know if our collecting work was successful.
+    # So we defined three situations: 
+    # 1. the number of files <or = or > the number of cases.
+    bg_url = None
+    remark = None
+    if type(files) is str:
+        files = [files]
+    # collect the links and filter the repetitive ones
+    if len(files) != len(set(files)):
+        file_url = []
+        for file in files:
+            if file not in file_url:
+                file_url.append(file)
     else:
-        return i
-
-
-def timestamp(date_str):
-    if not date_str:
-        return date_str
-    date_str = date_str.strip()
-    if date_str.isdigit():
-        if len(date_str) == 8:
-            tl = [date_str[0:4], date_str[4:6], date_str[6:8]]
+        file_url = files
+    if i and data_rows:
+        if len(file_url) == 1:
+            bg_url = file_url[0]  # 环评报告链接
+        elif len(file_url) == len(data_rows) - 1:
+            bg_url = file_url[i - 1]  # 环评报告链接
+        elif len(file_url) == 0:
+            bg_url = None
         else:
-            tl = []
+            remark = '附件数量（或顺序）有误，爬虫无法提取，请核对原网站后，再进行人工提取'
+    elif i and contents:
+        if len(file_url) == 1:
+            bg_url = file_url[0]  
+        elif len(file_url) == len(contents):
+            bg_url = file_url[i - 1]
+        elif len(file_url) == 0:
+            bg_url = None
+        else:
+            remark = '附件数量（或顺序）有误，爬虫无法提取，请核对原网站后，再进行人工提取'
     else:
-        tl = re.findall(r'\d+', date_str)
-    if len(tl) < 3:
-        return None
+        if len(file_url) == 1:
+            bg_url = file_url[0]  # 环评报告链接
+        elif len(file_url) == 0:
+            bg_url = None
+        else:
+            remark = '附件数量（或顺序）有误，爬虫无法提取，请核对原网站后，再进行人工提取'
+    return bg_url, remark # remarks contain the information of errors
+
+
+def get_files(response, file_texts, num):
+    '''
+    :param response: response signal from internet
+    :param file_texts: an iterable object, selected from tag <a>.
+    :param num: pages in need
+    :return: 1.msg represents the relation between the numbers of files and that of cases, 2. information of attached files
+    '''
+
+    texts = []  
+    files = []  
+    attachments = []  # format should be like：{'name': the text of the files, 'url': links}
+
+    if not isinstance(file_texts, list):
+        file_texts = [file_texts]
+
+    for file_text in file_texts:
+        if isinstance(file_text, Selector):
+            url = file_text.xpath('./@href').extract_first()
+            text = file_text.xpath('string(.)').extract_first(default='')
+            files.append(response.urljoin(url))
+            texts.append(text)
+        else:
+            if isinstance(file_text, str):
+                file_text = Selector(text='<a href="' + file_text + '"></a>')
+                url = file_text.xpath('//a/@href').extract_first()
+                text = file_text.xpath('string(.)').extract_first(default='')
+                files.append(response.urljoin(url))
+                texts.append(text)
+
+    for data in zip(texts, files):
+        dic = dict()
+        dic['name'] = data[0]
+        dic['url'] = data[1]
+        attachments.append(dic)
+
+    # duplicates the files when the content of the "name" and "url" of a same of an "attachment" is repetitive
+    if attachments:
+        attachments = reduce(lambda x, y: x if y in x else x + [y], [[], ] + attachments)
+
+    if len(file_texts) == 0:
+        msg = 1  # no files attached
+    elif len(file_texts) < num:
+        msg = 2  # files fewer than cases
+    elif len(file_texts) == num:
+        msg = 3  # files equal to cases
+    elif len(file_texts) > num:
+        msg = 4  # files more than cases
+
+    return msg, attachments
+
+
+def problem_item(basic_info, response, gs_type=None, error_info=None):
+    '''
+    :param gs_type: define the type of the notice
+    :param error_info: define the error message
+    :return:item
+    '''
+    item = HpspiderItem()
+    # content = response.xpath('//body').extract_first()
+    # item['p_type'] = gs_type
+    # item['p_content'] = content
+    # item['p_status'] = 0
+    item['problem_url'] = response.url
+    item['p_province'] = basic_info.get('province')  
+    item['p_city'] = basic_info.get('city')  
+    item['p_approval'] = basic_info.get('sp_bm')  
+    item['source_webname'] = basic_info.get('source_webname')   
+    item['error_info'] = error_info
+    return item
+
+
+def right_item(basic_info, response, gs_type=None):
+    '''
+    :param gs_type: define the type of the notice
+    :return: item
+    '''
+    item = HpspiderItem()
+    item['source_url'] = response.url 
+    item['province'] = basic_info.get('province')  
+    item['city'] = basic_info.get('city')  
+    item['sp_bm'] = basic_info.get('sp_bm')  
+    item['source_webname'] = basic_info.get('source_webname')   
+    item['gs_type'] = gs_type  
+    return item
+
+
+def merge_table(data_rows):
+    '''
+    This function rebuild a <table> tag to facilitate the scraping
+    data_list = []
+    for data_row in data_rows:
+        data_list.append(data_row.xpath('.').extract_first(default=''))
+
+    new_table_str = '<table>'
+    for tr in data_list:
+        new_table_str += tr
+    new_table_str += '</table>'
+    table = Selector(text=new_table_str)
+    return table
+
+
+def format_table(table):
+    '''
+    We from time to time encountered some merged cells in some HTML texts, and this method is aimed for that.
+    :param table: It should be a Selector.
+    :return: A table Selector without merged cells.
+    '''
+    data_list = []  # This initiated list is supposed to be two-dimension, reflecting the <tr>s and <td>s
+    table = table.xpath('.').extract_first(default='')
+    table = table.replace('<th', '<td').replace('</th', '</td').replace('<tbody>', '').replace('</tbody>', '') \
+        .replace('<thead>', '').replace('</thead>', '').replace('<tfoot>', '').replace('</tfoot>', '')
+    trs = Selector(text=table).xpath('//body/table/tr')
+
+    another_table = False
+    for i, tr in enumerate(trs):
+        tds = tr.xpath('./td')
+        for j, td in enumerate(tds):
+            has_rowspan = td.xpath('.//td/@rowspan')
+            has_colspan = td.xpath('.//td/@rowspan')
+            if has_rowspan or has_colspan:
+                another_table = True
+
+    if not another_table:
+        for i, tr in enumerate(trs):
+            data_list.append([])
+            tds = tr.xpath('./td')
+            for j, td in enumerate(tds):
+                data_list[i].append(td.xpath('.').extract_first())
+        # Insert the another piece of data into a blank according to the colspan/rowspan of the table
+        flag = True
+        while flag:
+            # While iterating <td>s in the data_list, extract the colspan/rowspan attribute of each tag.
+            # When colspan/rowspan >=2, clear the attribute
+            # Input the identical data into the next cell except the attribute colspan/rowspan (it has to be the former value minus 1
+            break_flag = False
+            for i, tds in enumerate(data_list):
+                for j, td in enumerate(tds):
+                    row = re.search('rowspan\s*?=\s*?"\s*?(\d+)\s*?"', td)
+                    col = re.search('colspan\s*?=\s*?"\s*?(\d+)\s*?"', td)
+                    if row and int(row.group(1)) > 1:
+                        rowspan = int(row.group(1))
+                        new_str_td = re.sub('rowspan\s*?=\s*?"\s*?\d+\s*?"', '', td)
+                        data_list[i][j] = new_str_td
+                        re_str_td = re.sub('rowspan\s*?=\s*?"\s*?(\d+)\s*?"',
+                                           'rowspan="' + str(rowspan - 1) + '"', td)
+                        try:
+                            data_list[i + 1].insert(j, re_str_td)
+                        except IndexError:
+                            pass
+                        break_flag = True
+                        break
+                    elif col and int(col.group(1)) > 1:
+                        colspan = int(col.group(1))
+                        new_str_td = re.sub('colspan\s*?=\s*?"\s*?\d+\s*?"', '', td)
+                        data_list[i][j] = new_str_td
+                        re_str_td = re.sub('colspan\s*?=\s*?"\s*?(\d+)\s*?"',
+                                           'colspan="' + str(colspan - 1) + '"', td)
+                        try:
+                            data_list[i].insert(j + 1, re_str_td)
+                        except IndexError:
+                            pass
+                        break_flag = True
+                        break
+                if break_flag:
+                    break
+            flag = False
+            break_flag = False
+            for i, tds in enumerate(data_list):
+                for j, td in enumerate(tds):
+                    row = re.search('rowspan\s*?=\s*?"\s*?(\d+)\s*?"', td)
+                    col = re.search('colspan\s*?=\s*?"\s*?(\d+)\s*?"', td)
+                    if row and int(row.group(1)) > 1 or col and int(col.group(1)) > 1:
+                        print(row, col)
+                        break_flag = True
+                        flag = True
+                        break
+                if break_flag:
+                    break
+
+        # build a table with all the data in the data_list
+        new_table_str = '<table>'
+        for i, rows in enumerate(data_list):
+            for j, data in enumerate(rows):
+                if len(rows) == 1:
+                    new_table_str += '<tr>' + data + '</tr>'
+                if len(rows) > 1:
+                    if j == 0:
+                        new_table_str += '<tr>' + data
+                    elif j == len(rows) - 1:
+                        new_table_str += data + '</tr>'
+                    else:
+                        new_table_str += data
+        new_table_str += '</table>'
+        table = new_table_str
     else:
-        tl = tl[:3]
-    ts = '-'.join(tl)
-    try:
-        date_time = int(time.mktime(time.strptime(ts, "%Y-%m-%d")))
-    except:
-        date_time = None
-    return date_time
-
-
-def hp_type_f(hp_type):
-    # This function is used to distinguish the type of environmental reports. 
-    # In general we have 3 types, including report file (报告书), report sheet (报告表) and (resgisteration sheet).
-    if not hp_type:
-        return hp_type
-    if '报告书' in hp_type:
-        return '报告书'
-    elif '报告表' in hp_type:
-        return '报告表'
-    elif '登记表' in hp_type:
-        return '登记表'
-    else:
-        return None
-
-
-def js_dw_hp_dw(s):
-    # js_dw is short for the Chinese pinyin of "建设单位", while hp_dw for "环评单位"
-    # So obviously both of them should be a 4-character-long string, or it is definitely wrong
-    ss = clean(s)
-    if not ss:
-        return ss
-    if len(ss) < 4:
-        return None
-    else:
-        return ss
-
-
-def re_project_name(pname):
-    # Many sites are likely to post many irrelevant stuff onto the pages, which is annoying to our job.
-    # So we tried to "clean" their language in following ways:
-    # cancel the "《》" and the meaningless Chinese words like "关于 (about/on)" and "环评 (the report of ...)"
-    if not pname:
-        return pname
-    # 第一把书名号去掉，在针对'项目'、‘工程’、‘环境影响’、‘环评’、‘批复’
-    pn0 = clean(pname)
-    pn1 = pn0.replace('《', '').replace('》', '')
-    pn2 = re.sub('(.*关于对)', '', pn1)
-    pn2 = re.sub('(.*关于)', '', pn2)
-    if '项目' in pn2:
-        tpn = re.search('(.*?项目)', pn2).group(1)
-    elif '工程' in pn2:
-        tpn = re.search('(.*?工程)', pn2).group(1)
-    elif '环评' in pn2:
-        tpn = re.search('(.*?)环评', pn2).group(1)
-    elif '批复' in pn2:
-        tpn = re.search('(.*?)批复', pn2).group(1)
-    else:
-        tpn = pn2
-
-    pn3 = re.sub('(环境影响.*)', '', tpn)
-    pn = re.sub('(\..[a-zA-Z]+)', '', pn3)    # 替换后缀
-
-    return pn
-
-
-def trip_item(item):
-    keys = ['project_name', 'gs_type', 'date_time', 'province', 'city', 'remark', 'status',
-            'source_url', 'sp_bm', 'sp_dh', 'hp_type', 'js_dw', 'hp_dw', 'site', 'xm_gk',
-            'yx_cs', 'gc_qk', 'bg_url', 'gc_url', 'pf_url', 'pf_content', 'pf_num', 'pf_time',
-            'non_sp', 'source_webname', 'problem_url', 'error_info'
-            ]
-
-    p_item = {}.fromkeys(keys, None)
-    p_item.update(item)
-    project_name = re_project_name(p_item['project_name'])
-    date_time = timestamp(p_item['date_time'])
-    sp_bm = clean(p_item['sp_bm'])  # dministrations(审批部门)
-    hp_type = hp_type_f(p_item['hp_type'])  # types of environemental reports(环评文件类型 报告书 报告表 登记表 未知)
-    js_dw = js_dw_hp_dw(p_item['js_dw'])  # Constructor(建设单位)
-    hp_dw = js_dw_hp_dw(p_item['hp_dw'])  # Evaluator of Environmental Effects(环评单位)
-    site = clean(p_item['site'])  # Construction site(建设地点)
-    pf_num = clean(p_item['pf_num'])  # Code of Response towards environmental evaluation(环评批复文号)
-    pf_time = timestamp(p_item['pf_time'])  # time of evaluation(环评批复时间)
-    p_item['project_name'] = project_name
-    p_item['date_time'] = date_time
-    p_item['sp_bm'] = sp_bm
-    p_item['hp_type'] = hp_type
-    p_item['js_dw'] = js_dw
-    p_item['hp_dw'] = hp_dw
-    p_item['site'] = site
-    p_item['pf_num'] = pf_num
-    p_item['pf_time'] = pf_time
-
-    # As the output of this function the dictionary is also the scraped information of this scrapy
-    return p_item
-
-
-def get_urls(item):
-    # We need to download the files attached in each web page.
-    # So we have to collect the urls where the files are located
-    # 'bg_url' is used to collect the address of report file(s), 'gc_url' notice file(s) and 'pf_url' response files.
-    data = item['data']
-    data = json.loads(data)
-    urls = []
-    if data['bg_url']:
-        urls.append(data['bg_url'])
-    if data['gc_url']:
-        urls.append(data['gc_url'])
-    if data['pf_url']:
-        urls.append(data['pf_url'])
-    return urls
-
-if __name__ == "__main__":
-    t = {
-            'project_name': '　　关于对广东万和电气有限公司新能源集成热水产品生2.5产基地扩产建设项目环境影响评价.pdf',
-            'date_time': '2018年07月20日',
-            'sp_bm': ' 绿 网 ', 'hp_type': 'xxx报告书',
-            'js_dw': ' jian she 单位', 'hp_dw': 'h品单位',
-            'pf_time': '20180720',
-    } # This should be defined by the coder himself/herself. So just one easy example.
-    tt = trip_item(t)
-    for k, v in tt.items():
-        if v:
-            print('%s: %s' % (k, str(v)))
+        pass
+    table = Selector(text=table)
+    return table
 
